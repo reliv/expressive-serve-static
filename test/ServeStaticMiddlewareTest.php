46b354b3756f134fb030a5a7b841d7a94b9702cb
rodmcnew <rodmcnew@gmail.com>
2017-10-18 18:13:16 -0500

    cleanup code and unit test

--- test/ServeStaticMiddlewareTest.php
+++ test/ServeStaticMiddlewareTest.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/../src/ContentTypes.php';
 
 use Interop\Http\ServerMiddleware\DelegateInterface;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
 use Reliv\ServeStatic\ServeStaticMiddleware;
 use Zend\Diactoros\Request;
 use Zend\Diactoros\Response;
@@ -35,23 +36,20 @@ class ServeStaticMiddlewareTest extends TestCase
     {
         $unit = new ServeStaticMiddleware(__DIR__ . '/public-test');
         $request = new ServerRequest([], [], 'https://example.com/test.json', 'GET');
-        $responseFromDelegate = new Response();
 
         $mockDelagate = $this->getMockBuilder(DelegateInterface::class)->getMock();
-        $mockDelagate->expects($this->once())
-            ->method('process')
-            ->with($request)
-            ->willReturn($responseFromDelegate);
 
+        /**
+         * @var $responseFromUnit ResponseInterface
+         */
         $responseFromUnit = $unit->process($request, $mockDelagate);
 
         $expectedFileContents = file_get_contents(__DIR__ . '/public-test/test.json');
 
-        var_dump($request->getBody()->getContents());
-
+        $responseFromUnit->getBody()->rewind();
         $this->assertEquals(
             $expectedFileContents,
-            $request->getBody()->getContents()
+            $responseFromUnit->getBody()->getContents()
         );
 
     }
