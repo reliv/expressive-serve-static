1fb1fafeeb7536abec484b45f748b2d9b08a046b
Tito Duarte <duartito@gmail.com>
2019-02-19 17:55:47 +0100

    [BUGFIX] - fixed direction of directory stack

--- src/ServeStaticMiddleware.php
+++ src/ServeStaticMiddleware.php
@@ -8,7 +8,6 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use SplDoublyLinkedList;
 use Zend\Diactoros\Response;
 use Zend\Diactoros\Stream;
 
